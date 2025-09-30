eea3708ba78377605e63da443952aa2f46834bf4
solaraengineer <solaradeveloper@gmail.com>
2025-09-30 10:48:02 +0200

    Fixed css rendering

--- config/settings.py
+++ config/settings.py
@@ -22,11 +22,14 @@ AUTH_USER_MODEL = 'core.User'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'core/static'),
+    BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Quick-start development settings - unsuitable for production
@@ -154,8 +157,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
